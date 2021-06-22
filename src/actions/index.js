1e44261699ce19396a95685382b1aea2037244e1
Lauren Nicholls <lauren.nicholls1@gmail.com>
2021-06-22 14:53:21 -0400

    set up basic templates

---
+++ src/actions/index.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const ROOT_URL = "https://api.openweathermap.org/data/2.5/forecast?";
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+
+export const FETCH_WEATHER = "FETCH_WEATHER";
+
+export function fetchWeather(city) {
+  const request = axios.get(
+    `${ROOT_URL}q=${city}&units=imperial&appid=${API_KEY}`
+  );
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request,
+  };
+}
