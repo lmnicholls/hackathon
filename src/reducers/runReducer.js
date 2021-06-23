3368a607ddd634fa5fdf79ae420d7998c71fe1cc
dalva19 <75229602+dalva19@users.noreply.github.com>
2021-06-23 13:54:10 -0400

    notRun component when conditions not met

--- src/reducers/runReducer.js
+++ src/reducers/runReducer.js
@@ -1,14 +1,21 @@
-import { GET_RUN } from "../actions";
+import { GET_RUN, GET_NOT_RUN } from "../actions";
 
 const DEFAULT_STATE = {
   idealTime: false,
+  notIdealTime: false,
 };
 
 const runReducer = (state = DEFAULT_STATE, action) => {
   switch (action.type) {
     case GET_RUN:
       return {
+        ...state,
         idealTime: true,
+      };
+    case GET_NOT_RUN:
+      return {
+        ...state,
+        notIdealTime: true,
       };
     default:
       return state;
