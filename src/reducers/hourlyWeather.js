1b8653ec8f24a7c33e3f2a2f712f16db0c24350a
Lauren Nicholls <lauren.nicholls1@gmail.com>
2021-06-22 20:31:25 -0400

    added HourlyWeatherReducer

---
+++ src/reducers/hourlyWeather.js
@@ -0,0 +1,43 @@
+import { GET_LOCATION } from "../actions";
+import { v4 as uuidv4 } from "uuid";
+import moment from "moment";
+
+const DEFAULT_STATE = {
+  hourlyWeather: [],
+};
+
+const HourlyWeatherReducer = function (state = DEFAULT_STATE, action) {
+  switch (action.type) {
+    case GET_LOCATION:
+      return {
+        hourlyWeather: [
+          {
+            id: uuidv4(),
+            date: action.payload.data.hourly.map((hour) => {
+              return moment
+                .utc((hour.dt + action.payload.data.timezone_offset) * 1e3)
+                .format("dddd, MMMM D, YYYY");
+            }),
+            time: action.payload.data.hourly.map((hour) => {
+              return moment
+                .utc((hour.dt + action.payload.data.timezone_offset) * 1e3)
+                .format("hh:mm A");
+            }),
+            temperature: action.payload.data.hourly.map((hour) => {
+              return hour.temp;
+            }),
+            humidity: action.payload.data.hourly.map((hour) => {
+              return hour.humidity;
+            }),
+            conditions: action.payload.data.hourly.map((hour) => {
+              return hour.weather[0].main;
+            }),
+          },
+        ],
+      };
+    default:
+      return state;
+  }
+};
+
+export default HourlyWeatherReducer;
