6c6e5c9135fa55e9bf04c0febb15b3f2ea0544e1
dalva19 <75229602+dalva19@users.noreply.github.com>
2021-06-25 09:35:27 -0400

    changed button color

--- src/App.js
+++ src/App.js
@@ -1,40 +1,15 @@
 import "./App.css";
 import React from "react";
-// import { Switch, Route } from "react-router-dom";
 //style
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 //components
-import Form from "./components/Form";
-//import DailyCharts from "./components/Daily_Charts";
-//import Run from "./components/Run";
-//import NotRun from "./components/Not-Run";
-// import CurrentWeather from "./components/Current_Weather";
-//import { getLocation } from "./actions";
+import Home from "./components/Home";
 
 function App() {
-  //const dispatch = useDispatch();
-
-  /*
-  useEffect(() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function (position) {
-        const { latitude } = position.coords;
-        const { longitude } = position.coords;
-
-        dispatch(getLocation(latitude, longitude));
-      });
-    } else {
-      alert("Sorry, your location was not found.");
-    }
-  }, []);*/
-
-  //const { idealTime, notIdealTime } = useSelector((state) => state.run);
-  //const { dataLoaded } = useSelector((state) => state.currentWeather);
-
   return (
     <div className="App">
-      <Form />
+      <Home />
     </div>
   );
 }
